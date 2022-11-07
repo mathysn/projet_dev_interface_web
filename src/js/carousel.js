fb9dfb129a548eb0b162ce4f6af2f1bf24310eb5
xeway <xeway@protonmail.com>
2022-11-07 20:13:52 +0100

    Carousel fonctionnel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,37 +3,43 @@ const sliderBtnLeft = document.querySelector("#slider-btn-left");
 const sliderBtnRight = document.querySelector("#slider-btn-right");
 let activeSlide = 1;
 
-// slides[activeSlide - 1].classList.add("active");
-
 function slideLeft(e) {
     activeSlide--;
+
+    let link = slides[activeSlide].getAttribute("id");
+
     if (activeSlide <= 1) sliderBtnLeft.style.display = "none";
 
     if (sliderBtnRight.style.display == "none" && activeSlide < slides.length) {
-        sliderBtnRight.style.display == "flex";
+        sliderBtnRight.style.display = "flex";
     }
 
-    sliderBtnLeft.setAttribute("href", `#slide-${activeSlide - 1}`);
-    sliderBtnRight.setAttribute("href", `#slide-${activeSlide + 1}`);
+    sliderBtnLeft.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) - 1}`);
+    sliderBtnRight.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) + 1}`);
 }
 
 function slideRight(e) {
     activeSlide++;
+
+    let link = slides[activeSlide - 2].getAttribute("id");
+
     if (activeSlide >= slides.length) sliderBtnRight.style.display = "none";
 
     if (sliderBtnLeft.style.display == "none" && activeSlide > 1) {
-        sliderBtnLeft.style.display == "flex";
+        sliderBtnLeft.style.display = "flex";
     }
 
-    sliderBtnLeft.setAttribute("href", `#slide-${activeSlide - 1}`);
-    sliderBtnRight.setAttribute("href", `#slide-${activeSlide + 1}`);
+    sliderBtnLeft.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) - 1}`);
+    sliderBtnRight.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) + 1}`);
 }
 
 window.addEventListener('load', (e) => {
     sliderBtnLeft.style.display = "none";
 
-    sliderBtnLeft.setAttribute("href", `#slide-${activeSlide - 1}`);
-    sliderBtnRight.setAttribute("href", `#slide-${activeSlide + 1}`);
+    let link = slides[activeSlide - 1].getAttribute("id");
+
+    sliderBtnLeft.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) - 1}`);
+    sliderBtnRight.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) + 1}`);
 });
 
 sliderBtnLeft.addEventListener("click", slideLeft);
