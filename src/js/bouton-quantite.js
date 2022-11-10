8d16e4472158c535ce91f28a6a94ad0413b269a2
xeway <xeway@protonmail.com>
2022-11-10 21:00:10 +0100

    Code clean

--- src/js/bouton-quantite.js
+++ src/js/bouton-quantite.js
@@ -1,14 +1,17 @@
+// variables import
 const addBtn = document.querySelector("#quantity-add");
 const removeBtn = document.querySelector("#quantity-remove");
 const output = document.querySelector("#quantity-output");
 let counter = 1;
 
+// increment counter when clicking on "add" button
 addBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     output.textContent = ++counter;
 });
 
+// decrement counter when clicking on "remove" button
 removeBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,4 @@
+// variables import
 const slides = document.querySelectorAll(".slide");
 const sliderBtnLeft = document.querySelector("#slider-btn-left");
 const sliderBtnRight = document.querySelector("#slider-btn-right");
@@ -5,11 +6,13 @@ let activeSlide = 1;
 
 const slidesContainer = document.querySelector("#slides");
 
+// change buttons' target link
 function changeLinksButtons(link) {
     sliderBtnLeft.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) - 1}`);
     sliderBtnRight.setAttribute("href", `#slide-${parseInt(link.slice(-1), 10) + 1}`);
 }
 
+// go to the previous image of the slider
 function slideLeft(e) {
     activeSlide--;
 
@@ -24,6 +27,7 @@ function slideLeft(e) {
     changeLinksButtons(link);
 }
 
+// go to the next image of the slider
 function slideRight(e) {
     activeSlide++;
 
@@ -38,6 +42,7 @@ function slideRight(e) {
     changeLinksButtons(link);
 }
 
+// prepare slider when user go on the page or refresh it
 window.addEventListener('load', (e) => {
     if (activeSlide <= 1) sliderBtnLeft.style.visibility = "hidden";
     if (activeSlide >= slides.length) sliderBtnRight.style.visibility = "hidden";
@@ -47,25 +52,30 @@ window.addEventListener('load', (e) => {
     changeLinksButtons(link);
 });
 
+// trigger slideLeft/slideRight when button is clicked
 sliderBtnLeft.addEventListener("click", slideLeft);
 sliderBtnRight.addEventListener("click", slideRight);
 
+// change image's scroll-snap-align css property
 function changeScrollSnapAlign(value) {
     slides.forEach(slide => {
         slide.style.scrollSnapAlign = value;
     });
 }
 
+// when mouse over button, trigger the animation to show a small part of the next/previous image in the slider
 function hoverEffect(cssClass, cssValue) {
     slidesContainer.classList.add(cssClass);
     changeScrollSnapAlign(cssValue);
 }
 
+// when mouse leave button, cancel the animation that show a small part of the next/previous image in the slider
 function leaveHoverEffect(cssClass, cssValue) {
     slidesContainer.classList.remove(cssClass);
     changeScrollSnapAlign(cssValue);
 }
 
+// events for right button
 sliderBtnRight.addEventListener("mouseover", (e) => {
     hoverEffect("right-btn-hovered", "start");
 });
@@ -73,6 +83,7 @@ sliderBtnRight.addEventListener("mouseout", (e) => {
     leaveHoverEffect("right-btn-hovered", "start");
 });
 
+// events for left button
 sliderBtnLeft.addEventListener("mouseover", (e) => {
     hoverEffect("left-btn-hovered", "end");
 });
