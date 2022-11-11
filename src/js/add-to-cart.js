6e9666a2deaba220f2b71e1d3d1da9dd2bb6a661
xeway <xeway@protonmail.com>
2022-11-11 18:44:08 +0100

    Modification add-to-cart.js

--- src/js/add-to-cart.js
+++ src/js/add-to-cart.js
@@ -1,6 +1,8 @@
 // increment the cart counter in the navbar
 document.querySelector("#add-cart-button").addEventListener("click", (e) => {
-    let cart = document.querySelector("a[href=\"cart.html\"]");
-    
-    cart.childNodes[1].textContent = parseInt(cart.textContent, 10) + 1;
+    let carts = document.querySelectorAll("a[href=\"cart.html\"]");
+
+    carts.forEach(cart => {
+        cart.childNodes[1].textContent = parseInt(cart.textContent, 10) + 1;
+    });
 });
