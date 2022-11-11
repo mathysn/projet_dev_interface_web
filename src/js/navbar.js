08f91ed9f423dadf33e97f0ea066f66029244ba4
Tom SIOUAN <102312082+JustSephix@users.noreply.github.com>
2022-11-11 15:19:45 +0100

    Update navbar.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,10 +1,6 @@
 const burgerMenu = document.querySelector(".nav-icon");
 const navLinks = document.querySelector(".res-navbar-links-container");
-var contener = document.getElementById('margin-32');
-
-contener.classList.remove("margin-32");
 
 burgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('ouvert');
-    contener.classList.add("margin-32");
 });
